b403538b8e5c1a30eb2aaeca125e9bcecf4b84fa
yein <yein.dev@gmail.com>
2025-10-02 23:13:18 +0900

    favicon 수정

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,7 +2,7 @@ import type { Favicon } from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
 	{
-		src: "/favicon/favicon.png",
+		src: "/favicon/favicon.ico",
 		theme: "light",
 		sizes: "32x32",
 	}
